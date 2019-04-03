3d8d4ccfe351ebfc13f86dafbf185b66cdc3ef50
Remy Dinh <remy_dinh@yahoo.com>
2019-04-03 01:46:13 -0700

    My two-level pointers work!

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <string.h> // for memcpy()
 #include <stdlib.h> // for srand()
 #include <time.h>
 
 
 // Utilities
 void callSorts(int* originalData);
-void printArray(int* intArray, unsigned const size);
-void swap(int* A, int* B);
+void swap(int** A, int** B);
 
 // Sorting Algorithms
-unsigned bubbleSort(int* intArray, unsigned size);
+unsigned bubbleSort(int** intArray, unsigned size);
 
 const unsigned ARRAYSIZE = 10;
 
@@ -19,11 +17,11 @@ int main (void)
     srand(time(0)); // Seeds the PRNG
 
     // Initialize the array with data
-    int myArray[ARRAYSIZE];
+    int coreData[ARRAYSIZE];
     for (unsigned i = 0u; i < ARRAYSIZE; i++)
-        myArray[i] = rand() % 50;
+        coreData[i] = rand() % 50;
 
-    callSorts(myArray);
+    callSorts(coreData);
     
     return 0;
 }
@@ -34,21 +32,38 @@ int main (void)
 ************************************************/
 void callSorts(int* originalData)
 {    
-    // Initialize a copy of the original data, to maintain data integrity.
-    int copyA[ARRAYSIZE];
-    memcpy(copyA, originalData, ARRAYSIZE * sizeof(int));
+    void printCopyArray(int** intArrayCopy, unsigned const size);
 
+    // Initialize a copy of the original data, 
+    // to maintain its integrity.
+    int* copyA[ARRAYSIZE];
     unsigned swapCount = 0;
+    for (unsigned i = 0u; i < ARRAYSIZE; i++)
+        copyA[i] = &originalData[i];
 
-    printf("- ORIGINAL ARRAY -\n");
-    printArray(originalData, ARRAYSIZE);
+
+    printf("- ORIGINAL DATA-\n");
+    printCopyArray(copyA, ARRAYSIZE);
     putchar('\n');
 
     swapCount = bubbleSort(copyA, ARRAYSIZE);
     
     printf("- Bubble SORTED -\n");
-    printArray(copyA, ARRAYSIZE);
+    printCopyArray(copyA, ARRAYSIZE);
     printf("Swaps performed: %u\n", swapCount);
+    putchar('\n');
+}
+
+/******************************************************
+  This function is designed to print an array of
+  pointers to int, rather than a raw array of ints
+  themselves. This is needed to print my copy arrays
+  which get sorted.
+******************************************************/
+void printCopyArray(int** intArrayCopy, unsigned const size)
+{
+    for (unsigned i = 0u; i < size; i++)
+        printf("%d ", **(intArrayCopy + i));
     putchar('\n');
 }
 
@@ -57,14 +72,14 @@ void callSorts(int* originalData)
    Applies a bubble sort, and prints out number
    of swaps applied to the data set.
 ************************************************/
-unsigned bubbleSort(int* intArray, unsigned size)
+unsigned bubbleSort(int** intArray, unsigned size)
 {
     unsigned swapCount = 0;
     for (unsigned j = 0; j < size - 1; j++)
     {
         for (unsigned k = 0 ; k < size - (j + 1); k++)
         {
-            if (intArray[k] > intArray[k + 1])
+            if (*(intArray[k]) > *(intArray[k + 1]))
             {
                 swap(&intArray[k], &intArray[k + 1]);
                 swapCount++;
@@ -74,23 +89,13 @@ unsigned bubbleSort(int* intArray, unsigned size)
     return swapCount;
 }
 
-/******************************************************
-   This function prints out an array in a single line.
-******************************************************/
-void printArray(int* intArray, unsigned const size)
-{
-    for (unsigned i = 0u; i < size; i++)
-        printf("%d ", intArray[i]);
-    putchar('\n');
-}
-
 /****************************************************
     This function is intended to be compatible with 
     various different sorts, not just a bubble sort.
 ****************************************************/
-void swap(int* A, int* B)
+void swap(int** A, int** B)
 {
-    int temp = *A;
+    int* temp = *A;
     *A = *B;
     *B = temp;
 }
